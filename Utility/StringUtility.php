23068475bd9733263acf007fe349b7386ff3a17b
Vic Cherubini <vmc@leftnode.com>
2013-01-02 03:33:12 -0600

    Added an uuid() method that returns a 4th generation UUID.

--- Utility/StringUtility.php
+++ Utility/StringUtility.php
@@ -26,7 +26,23 @@ class StringUtility
             $random .= $pool[mt_rand(0, $pool_len)];
         }
         
-        return($random);
+        return $random;
+    }
+
+    /**
+     * Generates a 4th generation UUID.
+     * Shamelessly taken from:
+     * http://us2.php.net/manual/en/function.uniqid.php#69164
+     *
+     * @return string
+     */
+    public function uuid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
     }
 
 }
